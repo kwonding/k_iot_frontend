5cd3925f4813b1a4b37d48bfc9e8a131f32457aa
kwonding <wldo8541@naver.com>
2025-10-30 12:38:04 +0900

    20251030 react

--- f-iot-react/src/pages/a_basic/index.tsx
+++ f-iot-react/src/pages/a_basic/index.tsx
@@ -8,6 +8,7 @@ import E_JSX from './E_JSX';
 import G_Props from './G_Props';
 import H_Props from './H_Props';
 import I_Rendering from './I_Rendering';
+import J_Handler from './J_Handler';
 
 const h2Style = {
   backgroundColor: 'black',
@@ -48,6 +49,9 @@ function Index() {
 
       <h2 style={h2Style}>5. Rendering: 조건부 렌더링</h2>
       <I_Rendering />
+
+      <h2 style={h2Style}>6. Handler: 리액트의 이벤트 핸들러</h2>
+      <J_Handler />
     </div>
   )
 }
