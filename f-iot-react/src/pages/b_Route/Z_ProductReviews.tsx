8fa649a79e3d1579bd49650789a111f6d7b06acb
kwonding <wldo8541@naver.com>
2025-11-06 11:35:59 +0900

    20251106 Products

---
+++ f-iot-react/src/pages/b_Route/Z_ProductReviews.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Z_ProductReviews() {
+  return (
+    <div>
+      <h4>사용자 리뷰</h4>
+      <ul>
+        <li>정말 마음에 들어요!♥️</li>
+        <li>배송이 빠르고 품질이 좋아요!</li>
+        <li>가성비가 좋아요👍</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Z_ProductReviews
